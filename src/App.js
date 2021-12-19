927070532065b25f69081613145302d94abb8986
iddimsangi <adammsangi@gmail.com>
2021-12-19 17:59:44 +0300

    added contents

--- src/App.js
+++ src/App.js
@@ -5,7 +5,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>helool</h1>
+      <div className="App-header--box">
+      <h1>
+      Get <span>insights</span> that help your business grow.
+      </h1>
+      <p>
+      Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.
+      </p>
+      <div className="footer">
+        <h2 className="footer--content">
+        10k+ <span>companies</span>
+        </h2>
+        <h2 className="footer--content">
+        314 <span>templates</span>
+        </h2>
+        <h2 className="footer--content">
+        12m <span>queries</span>
+        </h2>
+      </div>
+      </div>
+      <div className="App-header--box">
+        &nbsp;
+      </div>
       </header>
     </div>
   );
