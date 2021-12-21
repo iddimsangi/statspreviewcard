7017d81eabfe3534792b00f0c66ee0359bfee9e2
iddimsangi <adammsangi@gmail.com>
2021-12-21 14:31:10 +0300

    width and box height fixed

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <div className="App-header--box">
-      <h1>
-      Get <span>insights</span> that help your business grow.
-      </h1>
-      <p>
-      Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.
-      </p>
-      <div className="footer">
-        <h2 className="footer--content">
-        10k+ <span>companies</span>
-        </h2>
-        <h2 className="footer--content">
-        314 <span>templates</span>
-        </h2>
-        <h2 className="footer--content">
-        12m <span>queries</span>
-        </h2>
-      </div>
-      </div>
-      <div className="App-header--box">
-        &nbsp;
-      </div>
+        <div className="App-header--box">
+         <div className="wrapper">
+         <h1>
+            Get <span>insights</span> that help your business grow.
+          </h1>
+          <p>
+            Discover the benefits of data analytics and make better decisions
+            regarding revenue, customer experience, and overall efficiency.
+          </p>
+          <div className="footer">
+            <h2 className="footer--content">
+              10k+ <span>companies</span>
+            </h2>
+            <h2 className="footer--content">
+              314 <span>templates</span>
+            </h2>
+            <h2 className="footer--content">
+              12m+ <span>queries</span>
+            </h2>
+          </div>
+         </div>
+        </div>
+        <div className="App-header--box">&nbsp;</div>
       </header>
     </div>
   );
